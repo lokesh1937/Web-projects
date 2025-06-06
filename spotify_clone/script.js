4b41b356f568bab0fdc3dad97904eac750545a9a
lokesh1937 <lokeshbellamkonda36@gmail.com>
2025-06-06 17:47:19 +0530

    prefiles for spotify clone

---
+++ spotify_clone/script.js
@@ -0,0 +1,9 @@
+let loginbutton=document.querySelector(".loginbutton");
+loginbutton.addEventListener('click',function(){
+    window.location.href="login.html";
+});
+
+let signup_button=document.querySelector(".signuptag");
+signup_button.addEventListener('click',function(){
+    window.location.href="signup.html";
+})
